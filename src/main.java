312a016168f1cfe44a38122b8944ec00e969db32
Mariya <mari.tishina@internet.ru>
2023-04-04 23:19:56 +0300

    Bonus

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        int amount = 600;
+        int milya = 20;
+        int bonus = amount / milya;
+
+        System.out.println("Итоговые милли: " + bonus);
+    }
+
+}
